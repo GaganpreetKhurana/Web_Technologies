c26e22078644228e12f37e99e0d841c9a4661255
Gaganpreet Khurana <giftykhurana0@gmail.com>
2020-10-11 23:26:35 +0530

    A5 Q2 done

--- Assignment_4/Solution.java
+++ Assignment_4/Solution.java
@@ -4,6 +4,8 @@ class Solution {
     private static final String alphabets = "abcdefghijklmnopqrstuvwxyz";
     private static int N = 0;
     private static int M = 0;
+    private static int count = 0;
+    private static int minimum_changes = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
         int n, m;
@@ -18,57 +20,54 @@ class Solution {
         N = n;
         M = m;
         System.out.println(replant(crops));
+        System.out.println(count);
     }
 
     private static boolean isInValid(int x, int y) {
         return (x < 0 || x >= N || y < 0 || y >= M);
     }
 
-    private static int helper(String[] crops, int x, int y, char c, int changes) {
+    private static void helper(String[] crops, int x, int y, char c, int changes) {
+        count++;
         if (crops[x].charAt(y) != c) {
             changes++;
         }
+        if (changes >= minimum_changes) {
+            return;
+        }
         String new_string = crops[x].substring(0, y) + c + crops[x].substring(y + 1);
         String old_string = crops[x];
+//        System.out.println(new_string + " / " + old_string);
         crops[x] = new_string;
-        x += 1;
+        int current_row = x;
+        y += 1;
         if (isInValid(x, y)) {
-            x -= 1;
-            y += 1;
+            y = 0;
+            x += 1;
             if (isInValid(x, y)) {
-                crops[x] = old_string;
-                return changes;
+                crops[current_row] = old_string;
+                minimum_changes = changes;
+                return;
             }
         }
-        int temp_change = 0, min_change = Integer.MAX_VALUE;
         for (int i = 0; i < 26; i++) {
-            if ((!isInValid(x + 1, y) && alphabets.charAt(i) == crops[x + 1].charAt(y))
-                    || (!isInValid(x - 1, y) && alphabets.charAt(i) == crops[x - 1].charAt(y))
-                    || (!isInValid(x, y + 1) && alphabets.charAt(i) == crops[x].charAt(y + 1))
+            if ((!isInValid(x - 1, y) && alphabets.charAt(i) == crops[x - 1].charAt(y))
                     || (!isInValid(x, y - 1) && alphabets.charAt(i) == crops[x].charAt(y - 1))) {
                 continue;
             }
-            temp_change = helper(crops, x, y, alphabets.charAt(i), changes);
-            if (temp_change != Integer.MAX_VALUE) {
-                min_change = Math.min(temp_change, min_change);
-            }
+            helper(crops, x, y, alphabets.charAt(i), changes);
         }
-        crops[x] = old_string;
-        return min_change;
+        crops[current_row] = old_string;
     }
 
     public static int replant(String[] crops) {
         // Write your code here
         // Return the number of replanted crops
-        int min_changes = Integer.MAX_VALUE;
-        int changes = 0;
         for (int i = 0; i < 26; i++) {
-            changes = helper(crops, 0, 0, alphabets.charAt(i), changes);
-            min_changes = Math.min(min_changes, changes);
-            changes = 0;
+            helper(crops, 0, 0, alphabets.charAt(i), 0);
         }
 
-        return min_changes;
+        return minimum_changes;
 
     }
 }

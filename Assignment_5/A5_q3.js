1e2800ff98ba4e369f46b46d310d158d42f35ec5
Gaganpreet Khurana <giftykhurana0@gmail.com>
2020-11-06 02:09:37 +0530

    Completed q5.4

--- Assignment_5/A5_q3.js
+++ Assignment_5/A5_q3.js
@@ -1,34 +1,42 @@
 function validate_parenthesis() {
-    let string = prompt("Enter string:")
-    console.log(string)
-    let stack = []
+    let string = prompt("Enter string:") // Input
+
+    let stack = [] // Stack
     let dictionary = {
         "(": ")",
         "[": "]",
         "{": "}"
-    }
+    } // Dictionary for opposite pairs
+
     for (let i = 0; i < string.length; i++) {
         if (string[i] === "(" || string[i] === "[" || string[i] === "{") {
+            // Opening bracket
             stack.push(string[i])
         } else if (string[i] === ")" || string[i] === "]" || string[i] === "}") {
+            // Closing bracket
+
             if (stack.length === 0) {
+                // Stack empty
                 alert("Invalid")
                 return
             }
             let last = stack[stack.length - 1]
             if (string[i].localeCompare(dictionary[last]) === 0) {
+                // Pair matched
                 stack.pop()
             } else {
                 alert("Invalid")
                 return
             }
         } else {
+            // Invalid characters
             alert("Invalid Input!")
             return
         }
 
     }
     if (stack.length === 0) {
+        // Valid Parenthesis as Stack is empty
         alert("Valid")
     } else {
         alert("Invalid")
